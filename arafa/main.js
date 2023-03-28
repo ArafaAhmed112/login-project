6bebe2feb7f258017ef786db21e731bbd46ff6a7
ArafaAhmed112 <129184072+ArafaAhmed112@users.noreply.github.com>
2023-03-28 15:18:49 +0200

    Add files via upload

---
+++ arafa/main.js
@@ -0,0 +1,22 @@
+document.querySelectorAll(".input").forEach((input) => {
+  input.addEventListener("focus", (e) => {
+    const item = e.target;
+    const inputBox = item.closest(".input-box");
+    const icon = item.previousElementSibling;
+    item.style.color = "#fff";
+    inputBox.style.borderBottom = "2px solid #fff";
+    icon.style.color = "rgb(221,221,221)";
+  });
+
+  input.addEventListener("focusout", (e) => {
+    const item = e.target;
+    const inputBox = item.closest(".input-box");
+    const icon = item.previousElementSibling;
+    inputBox.style.borderBottom = "2px solid transparent";
+    icon.style.color = "";
+  });
+});
+
+document
+  .querySelector(".btn-login")
+  .addEventListener("click", (e) => e.preventDefault());
